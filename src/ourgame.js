d63eec505d39967556c3c6150d7dfee70288b782
Arnie Salazar <arsalazarjr@gmail.com>
2013-04-10 22:38:14 -0700

    Created guy object, with ability to move right

--- src/ourgame.js
+++ src/ourgame.js
@@ -1,8 +1,26 @@
 function init() {
-	var guy = document.createElement("img");
-	guy.src = "assets/sprites/guy.png";
-        guy.height=50;
-        guy.width=25;
-	container=document.getElementById("container");
-	container.appendChild(guy);
+	var guy = new Guy();
+	guy.moveRight();	
+	guy.render();
+}
+
+Guy = function() {
+	this.x = 0;
+	this.y = 0;
+
+	this.sprite = document.createElement("img");
+	this.sprite.style.position = "absolute";
+	this.sprite.src = "assets/sprites/guy.png";
+        this.sprite.height=50;
+        this.sprite.width=25;
+}
+
+Guy.prototype.render = function() {
+	var container = document.getElementById("container");
+	this.sprite.style.left = this.x + "px";
+	container.appendChild(this.sprite);
+}
+
+Guy.prototype.moveRight = function() {
+	this.x += 100;
 }
